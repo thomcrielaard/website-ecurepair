5e65ea72a6a3ad848d3bfdcd0cd4a70f6c541d9a
thomcrielaard <t.a.crielaard@gmail.com>
2024-06-30 01:13:12 +0200

    Updated ecu scraper

--- scripts/insert.py
+++ scripts/insert.py
@@ -8,100 +8,63 @@ strapi_url = "https://strapi.ecurepair.nl/api"  # Replace with your Strapi API U
 json_file_path = "translated_product_data_mercedes_contactslot.json"
 
 brand_id = 7
-part_id = 8
+part_id = 9
 
 # Open the JSON file for reading
 with open(json_file_path, "r") as json_file:
-    # Use json.load() to parse the JSON data into a Python data structure
     data = json.load(json_file)
 
-# # Function to create a brand in Strapi
-# def create_brand(brand_name):
-#   response = requests.post(
-#     f"{strapi_url}/merks",
-#     json={
-#       "data": {
-#         "naam": brand_name
-#       }
-#     }
-#   )
-#   return response
-
-# def get_brand(brand_name):
-#   response = requests.get(
-#     f"{strapi_url}/merks?filters[naam][$eq]={brand_name}"
-#   )
-
-#   return response
-
-
-# # Function to create a type in Strapi
-# def create_type(type_name):
-#   response = requests.post(
-#     f"{strapi_url}/types",
-#     json={
-#       "data": {
-#         "naam": type_name
-#       }
-#     }
-#   )
-#   return response
-
-# def get_type(type_name):
-#   response = requests.get(
-#     f"{strapi_url}/types?filters[naam][$eq]={type_name}"
-#   )
-
-#   return response
-
-
 # Function to create a module in Strapi
-def create_module(module_name, module_summary, module_description, brand_id, type_id):
+def create_product(part, product, brand_id, part_id):
+  # Extracting the last element from each part number's numbers list
+  onderdeelnummers = [part_['numbers'][-1] for part_ in product['part_numbers'] if 'numbers' in part_ and part_['numbers']]
+
+  # Joining the elements with a space
+  onderdeelnummer = ' '.join(onderdeelnummers) + ' ' + part['part_name']
+
+  formatted_onderdeelnummers = []
+  for nr in product["part_numbers"]:
+    title = nr["title"]
+    numbers = nr["numbers"]
+    for number in numbers:
+      # Voeg de geformatteerde string toe aan de lijst
+      formatted_onderdeelnummers.append(f"- {title} {number}")
+
+  # Voeg de strings samen met een newline karakter
+  onderdeelnummers_lijst = "\n".join(formatted_onderdeelnummers)
+
+  samenvatting = "Heeft u problemen met uw " + onderdeelnummer + "? Laat hem dan nu vervangen, repareren of reviseren door ECU Repair!"
+
+  omschrijving = "Bij ECU Repair kunt u uw {part_name} laten repareren, reviseren of vervangen. Onze specialisten zijn ervaren in het oplossen van problemen met dit onderdeel en andere soortgelijke onderdelen. Of het nu gaat om het herstellen van defecte componenten of het uitvoeren van preventief onderhoud, bij ECU Repair bent u verzekerd van een snelle en efficiënte service. Wilt u graag een afspraak maken? Vul dan nu het reparatieformulier in!\n##Onderdeelnummers\n" + onderdeelnummers_lijst
+
+  # Prepare faults list
+  fouten = [{"code": fault.get("fault_code"), "omschrijving": fault["fault_description"]} for fault in product["faults"]]
+
+  # Prepare cars list
+  autos = [{"model": car} for car in product["cars"]]
+
   response = requests.post(
     f"{strapi_url}/products",
     json={
       "data": {
-        "onderdeelnummer": module_name,
-        "samenvatting": module_summary,
-        "omschrijving": module_description,
-        "merk": brand_id,
-        "onderdeel": type_id
-      }
-    }
-  )
-  return response
-
-def get_module(module_name):
-  response = requests.get(
-    f"{strapi_url}/products?filters[onderdeelnummer][$eq]={module_name}"
-  )
-
-  return response
-
-def update_module(module_id, new_description):
-  response = requests.put(
-    f"{strapi_url}/products/{module_id}",
-    json={
-      "data": {
-        "omschrijving": new_description,
+        "onderdeelnummer": onderdeelnummer,
+        "samenvatting": samenvatting,
+        "omschrijving": omschrijving,
+        "merks": [brand_id],
+        "onderdeel": part_id,
+        "fouten": fouten,
+        "autos": autos
       }
     }
   )
   return response
 
 for part in data:
-  # Create new brand or get brand if it already exists
-  brand_response = create_module(part["part_name"], part["part_name"], part["part_name"], brand_id, part_id)
-#   if brand_response.status_code == 400:
-#     brand_response = get_brand(brand["brand_name"])
-#     brand_content = json.loads(brand_response.content)
-#     brand_id = brand_content["data"][0]["id"]
-#   else:
-#     brand_content = json.loads(brand_response.content)
-#     brand_id = brand_content["data"]["id"]
+  for product in part['products']:
+    product_response = create_product(part, product, brand_id, part_id)
 
-  if brand_response.status_code == 200:
-    print(f"Success for {part['part_name']}")
-  else:
-    print(f"Something went wrong with brand {part['part_name']}")
-    print(brand_response.content)
+    if product_response.status_code == 200:
+      print(f"Success for {product['part_numbers'][0]['numbers'][-1]}")
+    else:
+      print(f"Something went wrong with brand {part['part_name']}")
+      print(product_response.content)
--- scripts/translate_file.py
+++ scripts/translate_file.py
@@ -3,6 +3,8 @@ import os
 
 # Define file paths
 input_file_path = "detailed_product_data.json"
+preparse_file_path = "preparse.json"
+preparsed_output_file_path = "preparsed_product_data_mercedes_contactslot.json"
 translations_file_path = "translations.json"
 
 # Load or create the translations file
@@ -11,6 +13,13 @@ if os.path.exists(translations_file_path):
         translations = json.load(file)
 else:
     translations = {}
+
+# Load preparse file
+if os.path.exists(preparse_file_path):
+    with open(preparse_file_path, "r", encoding="utf-8") as file:
+        preparse = json.load(file)
+else:
+    preparse = {}
 
 # Function to collect texts that need translation
 def collect_texts_to_translate(texts, text):
@@ -28,8 +37,49 @@ def translate_text(text):
             json.dump(translations, file, ensure_ascii=False, indent=4)
         return translation
 
+# Function to preparse the faults descriptions
+def preparse_fault_descriptions(fault_code, fault_description):
+    key = f"{fault_code}: {fault_description}" if fault_code else fault_description
+    if key in preparse:
+        return [{"fault_code": fault_code, "fault_description": desc} for desc in preparse[key]]
+    else:
+        return [{"fault_code": fault_code, "fault_description": fault_description}]
+
 # Load the input JSON file
 with open(input_file_path, "r", encoding="utf-8") as file:
+    data = json.load(file)
+
+# Preparse the data
+preparsed_data = []
+for part in data:
+    parsed_part = {
+        "part_name": part["part_name"],
+        "image_url": part["image_url"],
+        "product_link": part["product_link"],
+        "products": []
+    }
+    for product in part.get("products", []):
+        parsed_product = {
+            "product_url": product["product_url"],
+            "part_numbers": product.get("part_numbers", []),
+            "faults": [],
+            "cars": product.get("cars", [])
+        }
+        for fault in product.get("faults", []):
+            fault_code = fault.get("fault_code", None)
+            fault_description = fault["fault_description"]
+            parsed_product["faults"].extend(preparse_fault_descriptions(fault_code, fault_description))
+        parsed_part["products"].append(parsed_product)
+    preparsed_data.append(parsed_part)
+
+# Save the preparsed data to a new JSON file
+with open(preparsed_output_file_path, "w", encoding="utf-8") as file:
+    json.dump(preparsed_data, file, ensure_ascii=False, indent=4)
+
+print(f"Preparsed data saved to {preparsed_output_file_path}")
+
+# Load the preparsed data for translation
+with open(preparsed_output_file_path, "r", encoding="utf-8") as file:
     data = json.load(file)
 
 # Collect texts that need translation
@@ -41,10 +91,12 @@ for part in data:
             collect_texts_to_translate(texts_to_translate, part_number["title"])
         for fault in product.get("faults", []):
             collect_texts_to_translate(texts_to_translate, fault["fault_description"])
+        for car in product.get("cars", []):
+            collect_texts_to_translate(texts_to_translate, car)
 
 # Print texts that need translation
 print("Texts that need translation:")
-for text in texts_to_translate:
+for text in sorted(texts_to_translate):
     print(text)
 
 # Translate the texts
@@ -59,9 +111,11 @@ for part in data:
             part_number["title"] = translate_text(part_number["title"])
         for fault in product.get("faults", []):
             fault["fault_description"] = translate_text(fault["fault_description"])
+        for i, car in enumerate(product.get("cars", [])):
+            product["cars"][i] = translate_text(car)
 
 # Save the translated data to a new JSON file
-output_file_path = "translated_product_data.json"
+output_file_path = "translated_product_data_mercedes_contactslot.json"
 with open(output_file_path, "w", encoding="utf-8") as file:
     json.dump(data, file, ensure_ascii=False, indent=4)
 
